d720ce7ffcbe32346deb297364857ae701adc4f6
Michael <miheinen@yahoo.de>
2018-07-10 19:10:14 +0200

    fhg

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if($(document).scrollTop() > 100)
     {
-      $('.navbar').addClass("navbar-toggled")
+      $('.navbar').addClass("navbar-toggled");
     } else {
-      $('.navbar').removeClass("navbar-toggled")
+      $('.navbar').removeClass("navbar-toggled");
     }
-  })
+  });
 
   //Navbar toggle
   $('.navbar-toggle').click(function(e){
-    e.preventDefault()
-    $('.navbar-collapse').toggleClass("navbar-show")
-  })
+    e.preventDefault();
+    $('.navbar-collapse').toggleClass("navbar-show");
+  });
 
 
   //Content Scroll Click
   $('#header-scroll').click(function(e){
-    e.preventDefault()
+    e.preventDefault();
     $('html, body').animate({
     scrollTop: $("#content").offset().top
     }, 1000);
-  })
+  });
 
-})
+});
